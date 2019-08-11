ba553812c4375b24d2183ff08ebe93e378d65781
Rupasharma18 <43802854+Rupasharma18@users.noreply.github.com>
2019-08-11 11:59:08 +0530

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -78,31 +78,3 @@ function sudokuPuzzle(puzzle){
 }
 let funCall = sudokuPuzzle(arrayData);
 console.log("solver Puzzle :-",funCall );
-=======
-//input data
-var unsolved = [
-    [0,0,0,2,6,0,7,0,1],
-    [6,8,0,0,7,0,0,9,0],
-    [1,9,0,0,0,4,5,0,0],
-    [8,2,0,1,0,0,0,4,0],
-    [0,0,4,6,0,2,9,0,0],
-    [0,5,0,0,0,3,0,2,8],
-    [0,0,9,3,0,0,0,7,4],
-    [0,4,0,0,5,0,0,3,6],
-    [7,0,3,0,1,8,0,0,0]
-];
-//console.log(unsolved[3][7]);
-//collecting indices of empty cells
-var empty = [];
-for(i=0;i<unsolved.length;i++){
-    for(j=0;j<unsolved.length;j++){
-        if(unsolved[i][j]===0){
-            empty.push([i,j]);
-        }
-    }
-}
-//console.log(empty);
-
-
-
->>>>>>> cbb294de18af8fb2b0896cca6d56ea0a1bc9f1d8
